662d579d1adbb6ec4de70af3ff8a4b6639e6caf3
RohaanoaZoro <rohaan.almeida@gmail.com>
2023-02-18 18:14:10 +0100

    Added MongoCluster Creds

--- swagger_server/service/dbconnect.py
+++ swagger_server/service/dbconnect.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 
 def MongoDatabase():
-    client = MongoClient('localhost', 27017)
+    client = MongoClient('mongodb+srv://cluster0.66vps.gcp.mongodb.net/myFirstDatabase', 27017,  username='Roal5809', password='Zxcvbnm@2')
     db = client.studentdb
     colz = db.student_service
     return colz
