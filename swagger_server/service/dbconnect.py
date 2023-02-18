7c7adb198b70c6574335770667f2ac92d962e6c3
RohaanoaZoro <rohaan.almeida@gmail.com>
2023-02-18 18:20:20 +0100

    Updated DB URL

--- swagger_server/service/dbconnect.py
+++ swagger_server/service/dbconnect.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 
 def MongoDatabase():
-    client = MongoClient('mongodb+srv://cluster0.66vps.gcp.mongodb.net/myFirstDatabase', 27017,  username='Roal5809', password='Zxcvbnm@2')
+    client = MongoClient('mongodb+srv://cluster0.cw2uej1.mongodb.net/myFirstDatabase', 27017,  username='Roal5809', password='Zxcvbnm@2')
     db = client.studentdb
     colz = db.student_service
     return colz
